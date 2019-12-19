170953ff30660a3ae8f6f256a58d98279200888d
yanmingdong <1779643401@qq.com>
2019-12-20 00:58:39 +0800

    添加自定义异常类

---
+++ oa/oa_dao/src/test/java/MyException.java
@@ -0,0 +1,8 @@
+public class MyException extends Exception {
+    private String message;
+
+    public MyException(String message) {
+        super(message);
+        this.message = message;
+    }
+}
